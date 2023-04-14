18789e3e1f7d4ccf01511d2fe3709bb515f2a61d
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-14 23:44:19 +0200

    Add create_book and create_rental methods

--- my_app/app.rb
+++ my_app/app.rb
@@ -110,4 +110,48 @@ def person_switch(choice, name, age)
 
   end
 end
+def create_book
+  print 'Enter book title:  '
+
+  title = gets.chomp
+
+  print 'Enter book author:  '
+
+  author = gets.chomp
+
+  book=Book.new(title,author)
+
+  @books << book
+  puts "Book created: #{book.title} by #{book.author}"
+end
+
+def create_rental
+  puts 'Select a book from the following list by number'
+
+  @books.each_with_index do |book, index|
+    puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+  end
+
+  selected_book_index = gets.chomp
+
+  selected_book = @books[selected_book_index.to_i]
+
+  puts 'Select a Person from the following list by number (not id)'
+
+  @people.each_with_index do |person, index|
+    puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  end
+
+  selected_person_index = gets.chomp
+
+  selected_person = @people[selected_person_index.to_i]
+
+  puts 'Enter date:'
+
+  date = gets.chomp
+
+  rental = Rental.new(date, selected_book, selected_person)
+
+  puts 'Rental created', rental
+end
 end

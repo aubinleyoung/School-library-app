17f74e4b9296ea563a159b0c7e5b5e7ff197425e
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-14 23:43:06 +0200

    Add create_person method

--- my_app/app.rb
+++ my_app/app.rb
@@ -44,4 +44,70 @@ class App
   
     puts 'No books in the library, create some.'
   end
+  
+def list_people(people)
+  puts 'All people:'
+
+  if people.length.positive?
+
+    people.each do |person|
+      if person.respond_to?('specialization')
+        puts "[TEACHER] Name: #{person.name.capitalize}, ID: #{person.id}, Age: #{person.age} }"
+      else
+        puts "[STUDENT] Name: #{person.name.capitalize}, ID: #{person.id}, Age: #{person.age} }"
+      end
+    end
+  else
+    puts 'No people in the school, create some.'
+  end
+end
+
+def create_person
+  puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+
+  person_type = gets.chomp.to_i
+  print 'Enter name:'
+  name = gets.chomp
+  print 'Enter age:'
+  age = gets.chomp
+  person_switch(person_type, name, age)
+end
+
+def person_switch(choice, name, age)
+  case choice
+
+  when 1
+
+    print 'Parent permission? [Y/N]:'
+
+    parent_permission = gets.chomp.downcase == 'y'
+
+    print 'Enter classroom:'
+
+    classroom = gets.chomp
+
+    new_student = Student.new(age, name, parent_permission,classroom)
+
+    @people << new_student
+
+    puts 'New Student created'
+
+  when 2
+
+    print 'Enter specialization:'
+
+    specialization = gets.chomp
+
+    new_teacher = Teacher.new(age, name, specialization)
+
+    @people << new_teacher
+
+    puts 'New Teacher created'
+
+  else
+
+    puts 'Invalid option'
+
+  end
+end
 end

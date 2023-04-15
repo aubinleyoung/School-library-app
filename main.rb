cb36981d0c766731b88098a2014617cd4dc8690e
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-15 12:28:31 +0200

    Fix linter errors

--- main.rb
+++ main.rb
@@ -5,7 +5,6 @@ list_options = ['1 - List all books', '2 - List all people', '3 - Create a perso
 
 def main(list_options)
   library = App.new(list_options)
-
   library.run
 end
 main(list_options)
